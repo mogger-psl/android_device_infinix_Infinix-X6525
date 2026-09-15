549cc3d79bcd3223e49da14ab643fd51ad2a089a
zevaaa <sebastiancordova172011@gmail.com>
2026-09-15 17:14:07 -0400

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -9,6 +9,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/crdroid_X6525.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_Infinix-X6525-user \
-    omni_Infinix-X6525-userdebug \
+    crdroid_X6525-user\
+    crdroid_X6525-userdebug \
     omni_Infinix-X6525-eng
